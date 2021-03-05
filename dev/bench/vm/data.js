b42b0311185089467d1936186ea5c84aaa1707aa
github-action-benchmark <github@users.noreply.github.com>
2021-03-05 20:40:36 +0000

    add Benchmark (benchmarkjs) benchmark result for c8908ea70ad3d04c0b629bee092101e17b15d36b

--- dev/bench/vm/data.js
+++ dev/bench/vm/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614937107390,
+  "lastUpdate": 1614976836261,
   "repoUrl": "https://github.com/MastaBook/ethereumjs-vm",
   "entries": {
     "Benchmark": [
@@ -186,6 +186,100 @@ window.BENCHMARK_DATA = {
             "name": "Block 9422914",
             "value": 1767,
             "range": "±2.03%",
+            "unit": "ops/sec",
+            "extra": "79 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Holger.Drewes@gmail.com",
+            "name": "Holger Drewes",
+            "username": "holgerd77"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c8908ea70ad3d04c0b629bee092101e17b15d36b",
+          "message": "VM, Block: large ChainID ecrecover fixes (#1139)\n\n* vm: fixed ecrecover call in ECRECOVER precompile for large chain IDs\r\n* block: fixed cliqueSigner() address recovery from signature for large chain IDs",
+          "timestamp": "2021-03-05T10:27:53-08:00",
+          "tree_id": "a1860ee25e53e702ed753232fbdd9792b3367be9",
+          "url": "https://github.com/MastaBook/ethereumjs-vm/commit/c8908ea70ad3d04c0b629bee092101e17b15d36b"
+        },
+        "date": 1614976835469,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Block 9422905",
+            "value": 1727,
+            "range": "±4.83%",
+            "unit": "ops/sec",
+            "extra": "79 samples"
+          },
+          {
+            "name": "Block 9422906",
+            "value": 1828,
+            "range": "±5.19%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "Block 9422907",
+            "value": 1720,
+            "range": "±7.74%",
+            "unit": "ops/sec",
+            "extra": "76 samples"
+          },
+          {
+            "name": "Block 9422908",
+            "value": 1818,
+            "range": "±2.24%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "Block 9422909",
+            "value": 1560,
+            "range": "±11.22%",
+            "unit": "ops/sec",
+            "extra": "71 samples"
+          },
+          {
+            "name": "Block 9422910",
+            "value": 1768,
+            "range": "±2.11%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "Block 9422911",
+            "value": 1734,
+            "range": "±2.23%",
+            "unit": "ops/sec",
+            "extra": "81 samples"
+          },
+          {
+            "name": "Block 9422912",
+            "value": 1676,
+            "range": "±2.30%",
+            "unit": "ops/sec",
+            "extra": "82 samples"
+          },
+          {
+            "name": "Block 9422913",
+            "value": 1657,
+            "range": "±2.32%",
+            "unit": "ops/sec",
+            "extra": "78 samples"
+          },
+          {
+            "name": "Block 9422914",
+            "value": 1424,
+            "range": "±9.78%",
             "unit": "ops/sec",
             "extra": "79 samples"
           }
