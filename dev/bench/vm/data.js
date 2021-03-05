533f3bc68c89f3ab4e4eeb03241553147ed158e1
github-action-benchmark <github@users.noreply.github.com>
2021-03-05 08:11:30 +0000

    add Benchmark (benchmarkjs) benchmark result for 562010578317aa871ffb86d5a38017a085fb7cb6

---
+++ dev/bench/vm/data.js
@@ -0,0 +1,102 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1614931890365,
+  "repoUrl": "https://github.com/MastaBook/ethereumjs-vm",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "christophergewecke@gmail.com",
+            "name": "cgewecke",
+            "username": "cgewecke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "562010578317aa871ffb86d5a38017a085fb7cb6",
+          "message": "Remove @types/bn.js from block,blockchain,trie,tx,vm packages (#1136)\n\n* deps -> remove @types/bn.js from block,blockchain,trie,tx,vm packages\r\n* vm -> import BN from ethereumjs-util everywhere\r\n* tx -> import BN from ethereumjs-util in TransactionFactory.ts",
+          "timestamp": "2021-03-04T19:07:53-08:00",
+          "tree_id": "2855768b4cd2b83560f1932ebcaf550ed13fe3c7",
+          "url": "https://github.com/MastaBook/ethereumjs-vm/commit/562010578317aa871ffb86d5a38017a085fb7cb6"
+        },
+        "date": 1614931889731,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Block 9422905",
+            "value": 1801,
+            "range": "±3.66%",
+            "unit": "ops/sec",
+            "extra": "79 samples"
+          },
+          {
+            "name": "Block 9422906",
+            "value": 1852,
+            "range": "±1.65%",
+            "unit": "ops/sec",
+            "extra": "82 samples"
+          },
+          {
+            "name": "Block 9422907",
+            "value": 1852,
+            "range": "±1.84%",
+            "unit": "ops/sec",
+            "extra": "79 samples"
+          },
+          {
+            "name": "Block 9422908",
+            "value": 1735,
+            "range": "±9.53%",
+            "unit": "ops/sec",
+            "extra": "77 samples"
+          },
+          {
+            "name": "Block 9422909",
+            "value": 1892,
+            "range": "±1.73%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          },
+          {
+            "name": "Block 9422910",
+            "value": 1903,
+            "range": "±1.52%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          },
+          {
+            "name": "Block 9422911",
+            "value": 1859,
+            "range": "±1.91%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          },
+          {
+            "name": "Block 9422912",
+            "value": 1836,
+            "range": "±1.88%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "Block 9422913",
+            "value": 1805,
+            "range": "±2.11%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "Block 9422914",
+            "value": 1740,
+            "range": "±2.43%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
