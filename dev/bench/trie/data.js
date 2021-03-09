52a7ae0547b9d50577a7292c05f007754285a78a
github-action-benchmark <github@users.noreply.github.com>
2021-03-09 12:49:41 +0000

    add Benchmark (benchmarkjs) benchmark result for 53150c159b559b31b6b7c321d784dbb11b082fb9

---
+++ dev/bench/trie/data.js
@@ -0,0 +1,95 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1615294181586,
+  "repoUrl": "https://github.com/MastaBook/ethereumjs-vm",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "Holger.Drewes@gmail.com",
+            "name": "Holger Drewes",
+            "username": "holgerd77"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "53150c159b559b31b6b7c321d784dbb11b082fb9",
+          "message": "Merge pull request #1130 from ethereumjs/add-new-rpc-methods\n\nClient: Add 6 new RPC methods",
+          "timestamp": "2021-03-09T11:56:38+01:00",
+          "tree_id": "fa1f03b097c5cb8d1669f94e7e06b44967f86647",
+          "url": "https://github.com/MastaBook/ethereumjs-vm/commit/53150c159b559b31b6b7c321d784dbb11b082fb9"
+        },
+        "date": 1615294180657,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "1k-3-32-ran",
+            "value": 13591,
+            "range": "±14.27%",
+            "unit": "ops/sec",
+            "extra": "63 samples"
+          },
+          {
+            "name": "1k-5-32-ran",
+            "value": 15416,
+            "range": "±4.74%",
+            "unit": "ops/sec",
+            "extra": "71 samples"
+          },
+          {
+            "name": "1k-9-32-ran",
+            "value": 14937,
+            "range": "±2.93%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
+          },
+          {
+            "name": "1k-1k-32-ran",
+            "value": 14117,
+            "range": "±3.70%",
+            "unit": "ops/sec",
+            "extra": "70 samples"
+          },
+          {
+            "name": "1k-1k-32-mir",
+            "value": 6801,
+            "range": "±26.54%",
+            "unit": "ops/sec",
+            "extra": "32 samples"
+          },
+          {
+            "name": "Checkpointing: 100 iterations",
+            "value": 1007,
+            "range": "±16.44%",
+            "unit": "ops/sec",
+            "extra": "62 samples"
+          },
+          {
+            "name": "Checkpointing: 500 iterations",
+            "value": 135,
+            "range": "±81.73%",
+            "unit": "ops/sec",
+            "extra": "50 samples"
+          },
+          {
+            "name": "Checkpointing: 1000 iterations",
+            "value": 98.59,
+            "range": "±37.94%",
+            "unit": "ops/sec",
+            "extra": "58 samples"
+          },
+          {
+            "name": "Checkpointing: 5000 iterations",
+            "value": 10.18,
+            "range": "±120.54%",
+            "unit": "ops/sec",
+            "extra": "26 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
